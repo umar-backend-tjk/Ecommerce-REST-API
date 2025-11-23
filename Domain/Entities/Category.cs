993dca093f5af75070d6b22b69e00c7ef57a379a
umar-backend-tjk <umarbackendtjk@mail.ru>
2025-11-23 23:11:42 +0500

    added BaseEntity and CategoryEntity

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Category : BaseEntity
+{
+    public required string Name { get; set; }
+    public required string Slug { get; set; }
+    public Guid? ParentCaregoryId { get; set; }
+    public int SortOrder { get; set; }
+}
