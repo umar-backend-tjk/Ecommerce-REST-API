d1700e64f21346c8d38ccba76ab9d7b79c9a583b
umar-backend-tjk <umarbackendtjk@mail.ru>
2025-12-01 17:50:42 +0500

    Added WishList and WishListItem entities (they weren't included earlier due to a bug)

---
+++ Domain/Entities/WishList.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class WishList
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CreatedAt = DateTime.UtcNow;
+    public List<WishListItem> Items { get; set; } = [];
+}
---
+++ Domain/Entities/WishListItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+public class WishListItem
+{
+    public Guid Id { get; set; }
+    public Guid WishListId { get; set; }
+    public WishList? WishList { get; set; }
+    public Guid ProductId { get; set; }
+    public Product? Product { get; set; }
+    public DateTime CreatedAt = DateTime.UtcNow;
+}
